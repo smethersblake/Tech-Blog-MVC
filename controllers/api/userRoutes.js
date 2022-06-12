4c9a24e4b6ba2afc220d3bcabe5e943d1e962cd4
Blake Smethers <smethersblake@gmail.com>
2022-06-12 13:25:40 -0500

    still cant get the contet of the post to show up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,7 +23,7 @@ router.get('/:id', (req, res) => {
         include: [
             {
                 model: Post,
-                attributes: ['id', 'title', 'created_at']
+                attributes: ['id', 'title', 'content', 'created_at']
             },
             {
                 model: Comment,
